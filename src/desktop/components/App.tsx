00380e75923dfe1ba3040dc24311af38b9ea05a3
Junyoung Choi <fluke8259@gmail.com>
2020-01-03 16:45:12 -0500

    Discard styled file for App and Router

--- src/desktop/components/App.tsx
+++ src/desktop/components/App.tsx
@@ -8,7 +8,6 @@ import { darkTheme } from '../themes/dark'
 import { lightTheme } from '../themes/light'
 import { sepiaTheme } from '../themes/sepia'
 import { solarizedDarkTheme } from '../themes/solarizedDark'
-import { StyledAppContainer } from './styled'
 import ContextMenu from './ContextMenu'
 import Dialog from './Dialog/Dialog'
 import { useDb } from '../lib/db'
@@ -26,7 +25,16 @@ import { useToast } from '../lib/toast'
 import { useUsers } from '../lib/accounts'
 import styled from '../lib/styled'
 
-export const LoadingText = styled.div`
+const AppContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+`
+
+const LoadingText = styled.div`
   margin: 30px;
 `
 
@@ -75,7 +83,7 @@ const App = () => {
   )
   return (
     <ThemeProvider theme={selectTheme(preferences['general.theme'])}>
-      <StyledAppContainer
+      <AppContainer
         onDrop={(event: React.DragEvent) => {
           event.preventDefault()
         }}
@@ -97,7 +105,7 @@ const App = () => {
         <Modal />
         <ToastList />
         <CodeMirrorStyle />
-      </StyledAppContainer>
+      </AppContainer>
     </ThemeProvider>
   )
 }
